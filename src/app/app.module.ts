a8f6bc05ec6841c87ddaf09c35e0ddc1d03520c5
naveen.2048@gmail.com <naveen.2048@gmail.com>
2018-10-07 15:09:35 -0500

    added snavigation with router-outlet and orders list page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OrdersListComponent } from './orders-list/orders-list.component';
+
+const routes: Routes = [
+  { path:'', redirectTo:'home', pathMatch:'full' },
+  { path:'home', component:HomeComponent },
+  { path:'orders', component:OrdersListComponent },
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OrdersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent],
