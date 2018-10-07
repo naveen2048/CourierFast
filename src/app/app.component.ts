de4271309b3ca3176f1c113aba720ea43aa08a27
naveen.2048@gmail.com <naveen.2048@gmail.com>
2018-10-07 14:44:08 -0500

    init - App using Shopify+Polaris framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+//import '@shopify/polaris/types/components/Page';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
@@ -13,6 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
